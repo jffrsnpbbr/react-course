15c53e503dabd2c8c79bc899525d6bd72571a7ad
Jefferson Pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-06-03 07:42:29 +0800

    Arrow Functions

--- javascript-refresher/index.js
+++ javascript-refresher/index.js
@@ -5,3 +5,25 @@ console.log(myName);
 
 myName = 'Manu';
 console.log(myName);
+
+// Arrow Functions
+
+function printMyName(name) {
+  console.log(name);
+}
+
+printMyName('Max');
+
+const printMyName2 = name => {
+  console.log(name);
+}
+
+const printMyNameAndAge = (name, age) => {
+  console.log(name, age);
+}
+
+printMyNameAndAge('Max', 28);
+
+const multiply = number => number * 2;
+
+console.log(multiply(2));
