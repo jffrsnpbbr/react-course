ede38b58521a53d71e54e8e76d09ebd0ee956733
Jefferson Pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-06-03 07:56:33 +0800

    Exports and imports

---
+++ javascript-refresher/app.js
@@ -0,0 +1,10 @@
+// default export
+import person from './person.js';
+import prs from './person.js';
+
+// named export
+
+import { baseData } from './utility.js';
+import { baseData as BaseData } from './utility.js';
+import { clean } from './utility.js';
+import * as bundled from './utility.js'
