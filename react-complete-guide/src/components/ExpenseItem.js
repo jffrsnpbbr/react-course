77eeb069192b16b34b32b713d379b4f7e7e351fb
Jefferson Pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-06-06 20:33:41 +0800

    adding basic css styling

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className='expense-item'>
       <div>June 6th, 2023</div>
-      <div>
+      <div className='expense-item__description'>
         <h2>Car Insureance</h2>
-        <div>$294.67</div>
+        <div className='expense-item__price'>$294.67</div>
       </div>
     </div>
   );
