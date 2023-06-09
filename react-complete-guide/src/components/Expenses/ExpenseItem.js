3df4fd120f80c0d3ef76bb58828e4cca3c3408b5
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-06-09 10:30:42 +0800

    listening to events and working with event handlers

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,21 @@
+import { useState } from 'react'
 import './ExpenseItem.css';
-
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = props => {
+  let [title,setTitle] = useState(props.title);
+  
+  const handleTitleChange = () => {
+    setTitle(title.split('').reverse().join(''))
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
+        <button onClick={handleTitleChange}>Change Title</button>
       </div>
     </Card>
   );
